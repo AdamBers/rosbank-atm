4e2cdb01ed2ea957c23eb412d8067615bef642bb
adam <adambers2020@gmail.com>
2023-03-02 14:33:13 +0300

    changed doughnuts

--- script.js
+++ script.js
@@ -27,3 +27,109 @@ new Chart(ctx1, {
         }
     }
 });
+
+
+
+
+
+//////////////////////////////////////////////// Chart-3
+const ctx3 = document.getElementById('chart-3');
+data3 = [1, 2, 1]
+backgroundColor1 = [
+    '#E40038',
+    '#001C3D',
+    '#D9D9D9'
+]
+
+
+new Chart(ctx3, {
+    type: 'doughnut',
+    data: {
+        labels: ["Оценка 1-2", "Оценка 3-4", "Оценка 5"],
+        datasets: [{
+            data: data1,
+            backgroundColor: backgroundColor1
+        }]
+    },
+    options: {
+        maintainAspectRatio: true,
+        responsive: true,
+        plugins: {
+            legend: {
+                display: false,
+            }
+        }
+    }
+});
+
+
+
+
+
+
+
+//////////////////////////////////////////////// Chart-7
+// const ctx7 = document.getElementById('chart-7');
+// data7 = [1, 2, 1]
+// backgroundColor1 = [
+//     '#E40038',
+//     '#001C3D',
+//     '#D9D9D9'
+// ]
+
+
+// new Chart(ctx7, {
+//     type: 'doughnut',
+//     data: {
+//         labels: ["Оценка 1-2", "Оценка 3-4", "Оценка 5"],
+//         datasets: [{
+//             data: data7,
+//             backgroundColor: backgroundColor1
+//         }]
+//     },
+//     options: {
+//         maintainAspectRatio: true,
+//         responsive: true,
+//         plugins: {
+//             legend: {
+//                 display: false,
+//             }
+//         }
+//     }
+// });
+
+
+
+
+
+
+
+//////////////////////////////////////////////// Chart-9
+const ctx9 = document.getElementById('chart-9');
+data9 = [1, 2, 1]
+backgroundColor9 = [
+    '#E40038',
+    '#001C3D',
+    '#D9D9D9'
+]
+
+
+new Chart(ctx9, {
+    type: 'doughnut',
+    data: {
+        labels: ["Оценка 1-2", "Оценка 3-4", "Оценка 5"],
+        datasets: [{
+            data: data9,
+            backgroundColor: backgroundColor9
+        }]
+    },
+    options: {
+        maintainAspectRatio: true,
+        responsive: true,
+        plugins: {
+            legend: {
+                display: false,
+            }
+        }
+    }
+});
